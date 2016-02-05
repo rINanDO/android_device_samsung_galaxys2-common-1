be2695ded9a0ad88cdecfdc69b304588f881e84e
Arnab Chaudhuri <coolguyforgenerationx@gmail.com>
2016-02-05 16:05:36 +0530

    galaxys2-common: Add new /system resizing script

--- releasetools/extensions/releasetools.py
+++ releasetools/extensions/releasetools.py
@@ -1,10 +1,7 @@
-import common
-import struct
-
 def FullOTA_PostValidate(info):
-	# run e2fsck
-	info.script.AppendExtra('run_program("/sbin/e2fsck", "-fy", "/dev/block/mmcblk0p9");');
-	# resize2fs: run and delete
-	info.script.AppendExtra('run_program("/tmp/install/bin/resize2fs_static", "/dev/block/mmcblk0p9");');
-	# run e2fsck
-	info.script.AppendExtra('run_program("/sbin/e2fsck", "-fy", "/dev/block/mmcblk0p9");');
+
+  info.script.AppendExtra(
+    ('ui_print("Resizing /system...");\n'
+    'run_program("/sbin/e2fsck_static", "-fy", "/dev/block/mmcblk0p9");\n'
+    'run_program("/sbin/resize2fs_static", "/dev/block/mmcblk0p9");\n'
+    'run_program("/sbin/e2fsck_static", "-fy", "/dev/block/mmcblk0p9");'))
